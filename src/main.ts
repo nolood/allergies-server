7dbdfb41a81575ba80d4dbaf954a31907190d7b4
nolood <a9514548799@gmail.com>
2023-11-14 18:58:13 -0800

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function start() {
   const PORT = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
 }
 
